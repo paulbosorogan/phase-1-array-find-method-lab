efe9f54380a5a51df9a79473934e6f035276417d
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-10-03 18:14:29 -0400

    commit tree f9686bce279e0c107b8a2ad31131c690b6bb6b42

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// const record = [
+//   { year: "2015",
+//     result: "W"},
+//   { year: "2014", 
+//     result: "N/A"},
+//]
+
+function superbowlWin(record){
+    for (let i=0; i < record.length; i++){
+        if(record[i].result === "W"){
+            return record[i].year}
+    }
+    return undefined
+}
+
+
+/*function superbowlWin(record){
+    if(record.result === "W")
+    return record.year
+}*/
+let found = record.find(record = record.result == "W")
